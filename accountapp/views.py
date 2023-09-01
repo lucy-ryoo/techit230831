98d797bd6ed42400fbe24423f2f81421b74ce5a5
ryoohyun <ryoohyunee@gmail.com>
2023-09-01 15:32:13 +0900

    submit button update

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
+    if request.method == "POST":
+        temp = request.POST.get('email')
+        return render(request, "accountapp/hello_world.html",
+                      context={"temp": temp})
+
     temp = "techit"
     return render(request, "accountapp/hello_world.html",
                   context={"temp": temp})
