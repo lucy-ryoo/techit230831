0e4203c277d55cdb23816711ddad32bd4c0d037a
ryoohyun <ryoohyunee@gmail.com>
2023-09-04 11:49:21 +0900

    createview (1)

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import Registration
 
@@ -29,3 +32,9 @@ def hello_world(request):
                   context={"temp": temp,
                            "reg_all": reg_all})
 
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy("accountapp:hello_world")
+    template_name = "accountapp/create.html"
