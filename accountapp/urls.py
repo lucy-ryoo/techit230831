c4b052a977799d360ad77559015893510c5e75a3
ryoohyun <ryoohyunee@gmail.com>
2023-09-04 14:17:39 +0900

    login view update

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountLoginView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path("hello_world/", hello_world, name="hello_world"),
 
-    path("create/", AccountCreateView.as_view(), name="create")
+    path("create/", AccountCreateView.as_view(), name="create"),
+    path("login/", AccountLoginView.as_view(), name="login")
 ]
