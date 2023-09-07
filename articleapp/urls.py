1a1ef1d245429178f56456b6a34ebbd1b3bb7593
ryoohyun <ryoohyunee@gmail.com>
2023-09-07 13:32:01 +0900

    magic grid picsum

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
+
+from articleapp.views import TempView
 
 app_name = 'articleapp'
 
 urlpatterns = [
+
+    path('temp/', TempView.as_view(), name="temp"),
 
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class TempView(TemplateView):
+    template_name = 'articleapp/temp.html'
