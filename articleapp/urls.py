59786902657fdce934e43531a66d0eb2d26c7dd5
ryoohyun <ryoohyunee@gmail.com>
2023-09-07 14:31:59 +0900

    articleapp detailview update

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from articleapp.views import TempView
+from articleapp.views import TempView, ArticleCreateView, ArticleDetailView
 
 app_name = 'articleapp'
 
 urlpatterns = [
 
     path('temp/', TempView.as_view(), name="temp"),
+    path('create/', ArticleCreateView.as_view(), name="create"),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name="detail"),
+
 
 ]
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,8 +1,31 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView, CreateView, DetailView
+
+from articleapp.forms import ArticleForm
+from articleapp.models import Article
 
 
 # Create your views here.
 
 class TempView(TemplateView):
     template_name = 'articleapp/temp.html'
+
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'articleapp/create.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail',
+                       kwargs={'pk': self.object.pk})
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = 'tqrget_article'
+    template_name = 'articleapp/detail.html'
